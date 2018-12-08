4035ad0e0eb815db78666a4d444f95ad7d7c76a6
EthanHans3n <1ethanhansen@gmail.com>
2018-12-07 20:43:52 -0800

    Added comments and formatting

--- main.kt
+++ main.kt
@@ -1,15 +1,18 @@
 fun main(args: Array<String>) {
+    //Making sure we have valid inputs
     if (args.size < 2) {
         println("Enter two numbers to find the ns between");
     } else if (args[0].toInt() < 1) {
         println("Please pick a start number greater than 0")
     } else if (args[1].toInt() < args[0].toInt()) {
         println("The end number must be greater than the start number")
-    }
-    else {
+    } else {    //We have valid inputs, so carry out calculations
+        
         val startNumber = args[0].toInt()
         val endNumber = args[1].toInt()
         var nArray: MutableList<Int> = mutableListOf(8)
+        
+        //Put all the n values for all the x and y (each) every value between start value and end value
         for (i in startNumber..endNumber) {
             for (j in startNumber..endNumber) {
                 nArray.add(6*i*j + i + j)
@@ -18,8 +21,9 @@ fun main(args: Array<String>) {
             }
         }
         
-        nArray.sort()
+        nArray.sort()   //Sort the array of all n's from smallest to largest
         
+        //Remove duplicate n values from the list
         var prev: Int = nArray[0]
         var k = 1
         while (k < nArray.size) {
@@ -30,6 +34,7 @@ fun main(args: Array<String>) {
             k++
         }
         
+        //Print the first value of every pair of consecutive n's
         for (m in 1..(nArray.size-2)){
             if (nArray[m] + 1 == nArray[m+1]) {
                 var first = nArray[m]
